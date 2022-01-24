7b172091ae9522991b37e7cca8af46e51255676e
gon-zo <pnw123456@icloud.com>
2022-01-24 20:03:14 +0900

    test: 유저 도메인 테스트 케이스 작성 (#2)

---
+++ src/test/java/com/example/order/domain/UserTest.java
@@ -0,0 +1,44 @@
+package com.example.order.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class UserTest {
+
+
+    @Test
+    @DisplayName("유저 도메인 테스트")
+    void userDTO() throws Exception {
+
+        String email = "test@naver.com";
+
+        String password = "12345";
+
+        String name = "user";
+
+        String nickName = "tester";
+
+        Integer phoneNumber = 0100000000;
+
+        User user = User.builder()
+                .email(email)
+                .password(password)
+                .name(name)
+                .nickName(nickName)
+                .phoneNumber(phoneNumber)
+                .build();
+
+        Assertions.assertEquals(email, user.getEmail());
+
+        Assertions.assertEquals(password, user.getPassword());
+
+        Assertions.assertEquals(name, user.getName());
+
+        Assertions.assertEquals(nickName, user.getNickName());
+
+        Assertions.assertEquals(phoneNumber, user.getPhoneNumber());
+
+    }
+
+}
