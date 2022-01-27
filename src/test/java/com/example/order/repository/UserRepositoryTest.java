d8044cbcc2c84375c6049f99cc54332bf8d1f9a6
gon-zo <pnw123456@icloud.com>
2022-01-27 23:21:37 +0900

    test: 단일 회원 상세 정보 조회 기능 - 성공 테스트 케이스 작성 (#6)
    - repository & service layer test case 작성

--- src/test/java/com/example/order/repository/UserRepositoryTest.java
+++ src/test/java/com/example/order/repository/UserRepositoryTest.java
@@ -3,6 +3,7 @@ package com.example.order.repository;
 import com.example.order.domain.User;
 import com.example.order.enums.Gender;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -16,6 +17,38 @@ class UserRepositoryTest {
 
     @Autowired
     private UserRepository userRepository;
+
+    private Long DEFAULT_ID = 0L;
+
+    private final String DEFAULT_EMAIL = "test@naver.com";
+
+    private final String DEFAULT_PASSWORD = "1234567890";
+
+    private final String DEFAULT_NAME = "가나다";
+
+    private final String DEFAULT_NICKNAME = "testetttaa";
+
+    private final String DEFAULT_PHONE_NUMBER = "010000000000";
+
+    private final Gender DEFAULT_GENDER = Gender.M;
+
+    @BeforeEach
+    void init() {
+
+        User entity = User.builder()
+                .email(DEFAULT_EMAIL)
+                .password(DEFAULT_PASSWORD)
+                .name(DEFAULT_NAME)
+                .nickName(DEFAULT_NICKNAME)
+                .phoneNumber(DEFAULT_PHONE_NUMBER)
+                .gender(DEFAULT_GENDER)
+                .build();
+
+        userRepository.save(entity);
+
+        DEFAULT_ID = entity.getId();
+
+    }
 
     @Test
     @DisplayName("유저 저장 로직")
@@ -64,6 +97,34 @@ class UserRepositoryTest {
 
         // 성별 체크
         Assertions.assertEquals(gender, entity.getGender());
+
+    }
+
+    @Test
+    @DisplayName("단일 회원 정보")
+    void findById() throws Exception {
+
+        User user = userRepository.findById(DEFAULT_ID).orElseThrow();
+
+        Assertions.assertEquals(DEFAULT_ID, user.getId());
+
+        // user email value checked
+        Assertions.assertEquals(DEFAULT_EMAIL, user.getEmail());
+
+        // 비밀번호 체크
+        Assertions.assertEquals(DEFAULT_PASSWORD, user.getPassword());
+
+        // 이름 체크
+        Assertions.assertEquals(DEFAULT_NAME, user.getName());
+
+        // 닉네임 체크
+        Assertions.assertEquals(DEFAULT_NICKNAME, user.getNickName());
+
+        // 전화번호 체크
+        Assertions.assertEquals(DEFAULT_PHONE_NUMBER, user.getPhoneNumber());
+
+        // 성별 체크
+        Assertions.assertEquals(DEFAULT_GENDER, user.getGender());
 
     }
 
--- src/test/java/com/example/order/service/UserServiceTest.java
+++ src/test/java/com/example/order/service/UserServiceTest.java
@@ -5,6 +5,7 @@ import com.example.order.enums.Gender;
 import com.example.order.repository.UserRepository;
 import com.example.order.service.dto.UserDTO;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +19,40 @@ class UserServiceTest {
 
     @Autowired
     private UserRepository userRepository;
+
+    private Long DEFAULT_ID = 0L;
+
+    private final String DEFAULT_EMAIL = "test@naver.com";
+
+    private final String DEFAULT_PASSWORD = "1234567890";
+
+    private final String DEFAULT_NAME = "가나다";
+
+    private final String DEFAULT_NICKNAME = "testetttaa";
+
+    private final String DEFAULT_PHONE_NUMBER = "010000000000";
+
+    private final Gender DEFAULT_GENDER = Gender.M;
+
+    private User entity;
+
+    @BeforeEach
+    void init() {
+
+        entity = User.builder()
+                .email(DEFAULT_EMAIL)
+                .password(DEFAULT_PASSWORD)
+                .name(DEFAULT_NAME)
+                .nickName(DEFAULT_NICKNAME)
+                .phoneNumber(DEFAULT_PHONE_NUMBER)
+                .gender(DEFAULT_GENDER)
+                .build();
+
+        userRepository.save(entity);
+
+        DEFAULT_ID = entity.getId();
+
+    }
 
     @Test
     @DisplayName("유저 회원 가입 테스트 케이스")
@@ -112,5 +147,12 @@ class UserServiceTest {
         User entity = userService.joinUser(userDTO);
 
         Assertions.assertNotNull(entity.getId());
+    }
+
+    @Test
+    @DisplayName("단일 회원 상세  정보 조회 기능")
+    void getUser() throws Exception {
+        User user = userService.getUser(DEFAULT_ID);
+        Assertions.assertEquals(user, entity);
     }
 }
