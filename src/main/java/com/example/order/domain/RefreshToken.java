9ad56c73778990f4e5164a3f8436c7e7fcc4ac11
gon-zo <pnw123456@icloud.com>
2022-02-01 12:30:07 +0900

    refactor: 리프레시 토큰 도메인 수정 (#12)

--- src/main/java/com/example/order/domain/RefreshToken.java
+++ src/main/java/com/example/order/domain/RefreshToken.java
@@ -4,11 +4,13 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.Hibernate;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.util.Objects;
 
 @Table
 @Getter
@@ -27,5 +29,23 @@ public class RefreshToken {
   public RefreshToken(Long userId, String refreshToken) {
     this.userId = userId;
     this.refreshToken = refreshToken;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+
+    if (this == o) return true;
+
+    if (!(o instanceof RefreshToken) || Hibernate.getClass(o) != Hibernate.getClass(this))
+      return false;
+
+    RefreshToken that = (RefreshToken) o;
+
+    return Objects.equals(that.userId, this.userId);
+  }
+
+  @Override
+  public int hashCode() {
+    return userId.intValue();
   }
 }
