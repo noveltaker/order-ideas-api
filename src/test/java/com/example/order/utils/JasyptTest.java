09a8e6de1a2ba3e9b60ec32f11af4cbdcce5b9de
gon-zo <pnw123456@icloud.com>
2022-02-01 16:30:39 +0900

    build: jasypt setting (#15)

--- src/test/java/com/example/order/utils/JasyptTest.java
+++ src/test/java/com/example/order/utils/JasyptTest.java
@@ -1,6 +1,7 @@
 package com.example.order.utils;
 
 //import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,25 +10,25 @@ public class JasyptTest {
   @Test
   @DisplayName("jasypt 값 인코딩 테스트")
   void encodeTest() {
-//    StandardPBEStringEncryptor jasypt = new StandardPBEStringEncryptor();
-//
-//    String[] array = new String[] {
-//            ""
-//    };
-//
-//    jasypt.setPassword("order112!!");
-//
-//    jasypt.setAlgorithm("PBEWithMD5AndDES");
-//
-//    for (String encrypt : array) {
-//
-//      String encryptedText = jasypt.encrypt(encrypt);
-//
-//      String text = jasypt.decrypt(encryptedText);
-//
-//      System.out.println("encryptedText:  " + encryptedText);
-//
-//      System.out.println("text:  " + text);
-//    }
+    StandardPBEStringEncryptor jasypt = new StandardPBEStringEncryptor();
+
+    String[] array = new String[] {
+            "etaskldfklncxmdsafvdsavcxzvxcv"
+    };
+
+    jasypt.setPassword("order112!!");
+
+    jasypt.setAlgorithm("PBEWithMD5AndDES");
+
+    for (String encrypt : array) {
+
+      String encryptedText = jasypt.encrypt(encrypt);
+
+      String text = jasypt.decrypt(encryptedText);
+
+      System.out.println("encryptedText:  " + encryptedText);
+
+      System.out.println("text:  " + text);
+    }
   }
 }
