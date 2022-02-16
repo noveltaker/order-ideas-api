2fe93d328a757016775cf15ed9fcd2ac2a432b82
gon-zo <pnw123456@icloud.com>
2022-02-16 18:43:59 +0900

    chore: local profile setting

--- src/main/java/com/example/order/config/H2Configuration.java
+++ src/main/java/com/example/order/config/H2Configuration.java
@@ -5,9 +5,11 @@ import org.h2.tools.Server;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import java.sql.SQLException;
 
+@Profile("local")
 @Configuration
 public class H2Configuration {
 
